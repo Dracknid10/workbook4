45c65fabfb5241429dad97e01d743dbb234d224f
Dracknid10 <55700825+Dracknid10@users.noreply.github.com>
2020-11-28 15:46:42 +0000

    started repo

---
+++ public/Index.php
@@ -0,0 +1,40 @@
+<?php
+
+include_once 'header.php';
+
+?>
+
+<h1>Stationery Application</h1>
+<p>Welcome to this fictional stationery
+    application page created for ISAD251 example. To continue please accept our terms and conditions</p>
+
+
+<body class="bg-info">
+<div class="container-fluid col-md-10 offset-md-1">
+    <div class="row">
+        <div class=’card mt-3 px-2 py-2’>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div>
+    <form action = "agreement.php" method="post">
+
+        <input type="radio" id="true" name="decision" value="true">
+        <label for="true">I agree</label><br>
+
+        <input type="radio" id="false" name="decision" value="false">
+        <label for="false">I disagree</label><br>
+
+        <button>submit</button>
+
+
+    </form>
+</div>
+
+</body>
+
+<?php include_once 'footer.php';?>
---
+++ public/agreement.php
@@ -0,0 +1,23 @@
+<body class="bg-info">
+<div class="container-fluid col-md-10 offset-md-1">
+    <div class="row">
+        <div class=’card mt-3 px-2 py-2’>
+
+        </div>
+
+    </div>
+
+</div>
+
+<?php
+
+$decision= $_POST ['decision'];
+
+echo 'You have selected ';
+echo $decision;
+echo ' for the agreement';
+
+
+?>
+
+</body>
