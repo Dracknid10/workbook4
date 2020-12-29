39c08f3c98134f6162e1864c408ae950d43d8ce8
Dracknid10 <55700825+Dracknid10@users.noreply.github.com>
2020-12-29 23:20:05 +0000

    changes made
    irrelevent

--- workbook4/workbook4/Models/COMP2001_JYarnoldContext.cs
+++ workbook4/workbook4/Models/COMP2001_JYarnoldContext.cs
@@ -29,11 +29,10 @@ namespace workbook4.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=socem1.uopnet.plymouth.ac.uk; Database=COMP2001_JYarnold; User ID=Jyarnold; Password=MaqC175*");
-            }
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    optionsBuilder.UseSqlServer("Server=socem1.uopnet.plymouth.ac.uk; Database=COMP2001_JYarnold; User ID=Jyarnold; Password=MaqC175*");
+            //}
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- workbook4/workbook4/Startup.cs
+++ workbook4/workbook4/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using workbook4.Models;
+
 
 namespace workbook4
 {
@@ -24,6 +27,10 @@ namespace workbook4
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<Models.COMP2001_JYarnoldContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("COMP2001_Jyarnold")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
